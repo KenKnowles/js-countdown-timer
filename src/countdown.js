ccf00cd8113909eb1744e1031e3ab2cc6b22374f
Kenneth Knowles <kknowles@knowles-mcniff.us>
2018-05-21 12:22:55 -0700

    Added formatting to resulting timer

--- src/countdown.js
+++ src/countdown.js
@@ -2,8 +2,9 @@
 
    $.fn.countdown = function() {
       return this.each(function(){
-         var xTimer = $(this);
-         xTimer.data("remaining", xTimer.attr("data-initial-time"));
+          var xTimer = $(this);
+          var initTime = xTimer.data("initial-time");
+          xTimer.data("remaining", initTime);
          xTimer.on("timer:tick", timer_handler);
       });
    };
@@ -18,12 +19,12 @@
       {
          if (remaining>0) {
             remaining = remaining - 1;
-            timer.html(remaining);
+            timer.html(formatTime(remaining));
          }
-         if (remaining<10)
+         if (remaining<300)
          {
             container.css("background-color","red");
-         } else if (remaining< 20)
+         } else if (remaining< 120)
          {
             container.css("background-color", "yellow");
          }
@@ -31,6 +32,39 @@
 
       timer.data("remaining", remaining);
    }
+
+
+    function formatTime(ms) {
+        var result = "";
+        var remaining = ms;
+        var sec = 1; // change this to 1000 if the parameter will be milliseconds
+        var min = sec * 60;
+        var hour = min * 60;
+        var day = hour * 24;
+
+        var days = Math.trunc(remaining / day);
+        remaining -= (days * day);
+
+        if (days > 0) result = days.toFixed(0) + ":";
+
+        var hours = Math.trunc(remaining / hour);
+        remaining -= (hours * hour);
+
+        if (hours > 0 || result.length > 0) {
+            result += (hours >= 10 ? "" : "0") + hours.toFixed(0) + ":";
+        }
+
+        var mins = Math.trunc(remaining / min);
+        remaining -= (mins * min);
+
+        result += (mins >= 10 ? "" : "0") + mins.toFixed(0) + ":";
+
+        var secs = Math.trunc(remaining / sec);
+
+        result += (secs >= 10 ? "" : "0") + secs.toFixed(0);
+
+        return result;
+    }
 
    var countdownInterval = setInterval(function(){
       $(".timer").trigger("timer:tick");
